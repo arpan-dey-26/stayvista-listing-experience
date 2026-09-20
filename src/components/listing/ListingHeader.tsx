4839ed89974159207e3a4e741da9639d12326390
Arpan Dey <ccmmnn2019@gmail.com>
2026-09-20 20:38:57 +0530

    Add src/components/listing/ListingHeader.tsx

---
+++ src/components/listing/ListingHeader.tsx
@@ -0,0 +1,5 @@
+import { ShareSaveActions } from './ShareSaveActions';
+
+export function ListingHeader({ title }: { title: string }) {
+  return <div className="listing-header"><h1 className="listing-header__title">{title}</h1><div className="listing-header__actions"><ShareSaveActions title={title} /></div></div>;
+}
